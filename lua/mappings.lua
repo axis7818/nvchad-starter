83439272577d532d7b5f4558aef1de4b2cb4fa9a
Cameron Taylor <axis7818@gmail.com>
2025-02-17 06:23:34 -0800

    tmux vim navigation

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,10 +6,15 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- tmux window navigation
+map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "window left" })
+map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "window down" })
+map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "window up" })
+map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "window right" })
+
 map("n", "<leader>\\", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
 map("n", "<C-n>", "<cmd>nohl<CR>", { desc = "disable search highlighting" })
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
